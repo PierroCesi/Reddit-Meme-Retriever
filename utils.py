a63f402b1a84a360c6cd3eec09c67d219e892c71
PierroCesi <pierre.forques@viacesi.fr>
2022-12-12 15:36:49 +0100

    update utils

--- utils.py
+++ utils.py
@@ -2,7 +2,9 @@ import os
 import json
 
 FOLDER_NAME = "memes"
-SUBREDDIT_PATH = "subreddit.json"
+SUBREDDIT_PATH = "ressources/subreddit.json"
+TELEGRAM_PATH = "ressources/telegram.json"
+USER_PATH = "ressources/user_data.json"
 
 def init():
     if not os.path.exists("./"+FOLDER_NAME):
@@ -15,3 +17,15 @@ def get_subreddit_list():
 def get_reddit_authentification():
     with open(SUBREDDIT_PATH, 'r') as f:
         return json.load(f)['authentification']
+
+def get_telegram_info():
+    with open(TELEGRAM_PATH, 'r') as f:
+        return json.load(f)['telegram']
+
+def get_telegram_channel_list():
+    with open(TELEGRAM_PATH, 'r') as f:
+        return json.load(f)['telegram_channel']
+
+def get_telegram_users():
+    with open(USER_PATH, 'r') as f:
+        return json.load(f)
